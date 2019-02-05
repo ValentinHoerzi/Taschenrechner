f776ae2f4bd49bddc824f67c592d63b8f44e918a
ValentinHoerzi <valentin.hoerzi@gmail.com>
2019-02-05 15:33:16 +0100

    new commit

--- app/src/main/java/htl/grieskirchen/androidstudio/taschenrechner/PostfixUtil.java
+++ app/src/main/java/htl/grieskirchen/androidstudio/taschenrechner/PostfixUtil.java
@@ -1,4 +1,55 @@
 package htl.grieskirchen.androidstudio.taschenrechner;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
 public class PostfixUtil {
+    private PostfixUtil() {
+    }
+
+    public static List<String> infixToPostfix(List<String> infix){
+
+        return null;
+    }
+
+    private static boolean isNumber(String token) {
+       try{
+           Double.parseDouble(token);
+       }catch(Exception r){
+           return false;
+       }
+        return true;
+    }
+
+    public static BigDecimal evaluate(List<String> postfix){
+        return null;
+    }
 }
+/*
+      -> Leerzeichen bei Operatoren, nicht bei uzahlen -> -(-7-+-11-)-*-4 dann split nach " "
+      -> 7 11 + 4 *
+      dann entscheidungsprozess -> ist 7 zahl? stack 1, nach 2 zahlen operator verlangt und ergebnis in stack
+      minus zahlen in klammer
+      Max lösung zu ungültige eingabe -> schauen, ob der nutzer 2 operanten hintereinander eingegeben hat
+      ^[1-9]+\.?[0-9]*$ -> kontrolliert ob es eine gültige zahl ist
+
+             List<String> out = new ArrayList<>();
+        for (int i = 0; i < infix.size(); i++) {
+            String token = infix.get(i);
+            if(isNumber(token)){
+                out.add(token);
+            }
+            if(isOperator(token)){
+                whlie (pop-operator > operator)
+                        pop-operator -> out
+                      push operator
+                      if(token = "(" push "(")
+                          if token ")"
+                    while !"(" pop -> out
+                   while !stack.empty pop -> out
+            }
+        }
+ */
--- app/src/test/java/htl/grieskirchen/androidstudio/taschenrechner/ExampleUnitTest.java
+++ app/src/test/java/htl/grieskirchen/androidstudio/taschenrechner/ExampleUnitTest.java
@@ -2,6 +2,11 @@ package htl.grieskirchen.androidstudio.taschenrechner;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,7 +16,45 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testPostfix1() {
+        List<String> exp = Arrays.asList("2","2","+");
+        List<String> result = PostfixUtil.infixToPostfix(Arrays.asList("2", "+", "2"));
+        assertEquals(exp,result);
+    }
+
+    @Test
+    public void testPostfix2() {
+        List<String> exp = Arrays.asList("2","2","+");
+        List<String> result = PostfixUtil.infixToPostfix(Arrays.asList("2", "+", "2"));
+        assertEquals(exp,result);
+    }
+
+    @Test
+    public void testPostfix3() {
+        List<String> exp = Arrays.asList("2","2","+");
+        List<String> result = PostfixUtil.infixToPostfix(Arrays.asList("2", "+", "2"));
+        assertEquals(exp,result);
+    }
+
+    @Test
+    public void textCalc1(){
+        BigDecimal exp = new BigDecimal(12);
+        List<String> result = Arrays.asList("2", "2", "+");
+        assertEquals(exp, result);
+    }
+
+    @Test
+    public void textCalc2(){
+
+    }
+
+    @Test
+    public void textCalc3(){
+
+    }
+
+    @Test
+    public void textCalc4(){
+
     }
 }
